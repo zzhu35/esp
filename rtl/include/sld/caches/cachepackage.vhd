27f85dde59dd9790ff30b033733e73740369ed38
zzhu35 <zzhu35@illinois.edu>
2021-01-30 16:11:13 -0600

    Update cachepackage.vhd

--- rtl/include/sld/caches/cachepackage.vhd
+++ rtl/include/sld/caches/cachepackage.vhd
@@ -5,7 +5,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use work.esp_global.all;
-use work.spandex_config.all;
 use work.amba.all;
 use work.stdlib.all;
 use work.gencomp.all;
